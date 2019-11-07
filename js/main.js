680d75f0945ae36c984643136d24167df4ebc5b5
nacim harfouche <nacim_4@hotmail.fr>
2019-11-07 01:14:50 +0100

    regular expression test

--- js/main.js
+++ js/main.js
@@ -2,19 +2,20 @@ let a = "firstName lastName test test test";
 let array = [];
 
 let first = {
-    1: "&",
-    2: "5",
-    3: "/",
-    4: "+",
-    5: "-"
+    a: "µ",
+    b: "5",
+    c: "/",
+    d: "+",
+    e: "-"
 }
 
 
 a = a.replace(/\s/g, "~")
-console.log(a)
+// console.log(a)
 
-// a = a.replace(/a/g, `a${first.1}b${first.2}c${first.3}d${first.4}e${first.5}`);
-console.log(first);
+a = a.replace(/[a]{1}/g, `r${first.a}b${first.b}c${first.c}d${first.d}e${first.e}`);
+// console.log(a)
+
 
 for (let i = 0; i < a.length; i++) {
     array.push(a.charAt(i));
@@ -22,9 +23,22 @@ for (let i = 0; i < a.length; i++) {
         b = 0
         if (letter === "~") {
             b++
-            console.log(b)
+            // console.log(b)
         }
     }
 }
+// console.log(array)
 
-console.log(array)
+/*----------------------------------------------*/
+
+let test2 = "test text dghfghfGaGfdghfgh words";
+
+test2 = test2.replace(/[a]{1}/g, `r${first.a}b${first.b}c${first.c}d${first.d}e${first.e}`);
+
+let regex = RegExp(`[.${first.a}.${first.b}.${first.c}.${first.d}.${first.e}]{1}`, "g");
+
+if (regex.test(test2)) {
+    console.log("yes");
+    test2 = test2.replace(RegExp, `a`);
+    console.log(test2);
+}
